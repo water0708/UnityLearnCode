bd9f7abbfbd58a0456dce12865da5d4e7d0e005a
water0708 <51389170+water0708@users.noreply.github.com>
2019-06-05 11:41:13 +0800

    新增狗類別欄位與叫的方法

---
+++ UnityLearnCode/Assets/Scripts/Dog.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+public class Dog
+{
+    #region 欄位
+    public string name;
+    public float weight;
+    public string type;
+    public string sex;
+    public string color;
+    public int age;
+    #endregion
+
+    #region 方法
+    public void Bark()
+    {
+        Debug.Log(name + ":汪汪!!!");
+    }
+    #endregion
+}
