4fa0640a1f309706720c836ce04f1b1015ebc390
water0708 <51389170+water0708@users.noreply.github.com>
2019-06-05 13:12:18 +0800

    方法與多載方法

--- UnityLearnCode/Assets/Scripts/Dog.cs
+++ UnityLearnCode/Assets/Scripts/Dog.cs
@@ -11,9 +11,35 @@ public class Dog
     #endregion
 
     #region 方法
+    /// <summary>
+    /// 狗叫的方法
+    /// </summary>
     public void Bark()
     {
         Debug.Log(name + ":汪汪!!!");
+    }
+    /// <summary>
+    /// 狗狗轉換年齡的方法
+    /// </summary>
+    /// <returns>狗狗年齡*7轉化成為人類年齡</returns>
+    public int ConvertAge()
+    {
+        return age * 7;
+    }
+    #endregion
+    #region
+    /// <summary>
+    /// 狗吃飯的方法
+    /// </summary>
+    /// <param name="food"></param>
+    /// <param name="speed"></param>
+    public void Eat(string food, string speed = "慢")
+    {
+        Debug.Log(name + ":" + food + ",速度:" + speed);   
+    }
+    public void Eat(string food, int count)
+    {
+        Debug.Log(name + ":" + food + ",份量:" + count);
     }
     #endregion
 }
